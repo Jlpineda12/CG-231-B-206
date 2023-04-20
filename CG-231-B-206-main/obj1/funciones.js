c3bdee6927d0d8f0f150dc1f1ea58b3f5fe87dce
Jlpineda12 <123666863+Jlpineda12@users.noreply.github.com>
2023-04-20 15:28:35 -0500

    Ultima actualizacion parcial

--- CG-231-B-206-main/obj1/funciones.js
+++ CG-231-B-206-main/obj1/funciones.js
@@ -1,17 +1,60 @@
-//Funcion de traslacionTHREE.
-
-
-
+//Funciones de traslacion 
+function traslacionX(obj)
+{
+	obj.translateX(Tx);
+	return obj;
+}
+function traslacionY(obj)
+{
+	obj.translateY(Ty);
+	return obj;
+}
+function traslacionZ(obj)
+{
+	obj.translateZ(Tz);
+	return obj;
+}
+//Funciones de rotacion
+function RotacionX(obj)
+{
+	obj.rotation.x +=Rx;
+	return obj;
+}
+function RotacionY(obj)
+{
+	obj.rotation.y +=Ry;
+	return obj;
+}
+//Funciones de escalado
+function EscaladoY(obj)
+{
+	obj.scale.setY(sy);
+	return obj;
+}
+function EscaladoX(obj)
+{
+	obj.scale.setX(sx);
+	return obj;
+}
+function EscaladoZ(obj)
+{
+	obj.scale.setZ(sz);
+	return obj;
+}
 
 function transformaciones(esfera)//Esta funcion se encargara de realizar todas las transformaciones requeridas del parcial
 {
-	esfera.scale.setX(sx);
-	esfera.scale.setY(sy);
-	esfera.scale.setZ(sz);
-	esfera.translateY(Ty);
-	esfera.rotation.x +=Rx;
-	esfera.rotation.y += -Ry;
-	esfera.translateX(Tx);
+	EscaladoX(esfera);
+	EscaladoY(esfera)
+	EscaladoZ(esfera)
+	//Obtenemos la primera imagen 
+	RotacionX(esfera);
+	RotacionY(esfera);
+	traslacionY(esfera);
+	
+	traslacionZ(esfera);
+	//Obtenemos la imagen f
+
 	return esfera;
 }
 
--- CG-231-B-206-main/obj1/main.js
+++ CG-231-B-206-main/obj1/main.js
@@ -2,10 +2,11 @@ var radio=1;
 var sx=0.5;
 var sy=0.5;
 var sz=3;
-var Rx=Math.PI / 2;
-var Ry=Math.PI / 3;
-var Ty=radio*5.9;
+var Rx=Math.PI / 8;
+var Ry=-Math.PI / 8;
+var Ty=-1/26*radio;
 var Tx=5.1*radio;
+var Tz=-3*radio;
 
 var WIDTH = window.innerWidth;
 var HEIGHT = window.innerHeight;
@@ -20,11 +21,11 @@ var scene = new THREE.Scene();
 
 
 var camera = new THREE.PerspectiveCamera(80, WIDTH / HEIGHT);
-camera.position.z = 4.5;
-camera.position.x = -1.2;
-camera.position.y = 2;
+camera.position.z = 10;
+camera.position.x = 3;
+camera.position.y = 4;
 
-camera.rotation.set(0, -0.5, 0);
+camera.rotation.set(0, -0.1, 0);
 scene.add(camera);
 
 
@@ -34,14 +35,7 @@ var geometry = new THREE.SphereGeometry(radio,32,16);
 var material = new THREE.MeshBasicMaterial( {color: 0xFCF3CF } );
 var esfera = new THREE.Mesh( geometry, material );
 scene.add(esfera)
-/*esfera.scale.setX(sx);
-esfera.scale.setY(sy);
-esfera.scale.setZ(sz);
-esfera.translateY(Ty);
-esfera.rotation.x +=Rx;
-esfera.rotation.y += -Ry;
-esfera.translateX(Tx);
-*/
+
 transformaciones(esfera);
 
 //funciones
